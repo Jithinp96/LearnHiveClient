29a8a9e42d45773b0fe8f942aa0fffa2b8fb0be2
Jithin <jithinpganesh@outlook.com>
2024-11-20 18:11:53 +0530

    Socket url changed

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_URL = 'http://localhost:5000'
+const SOCKET_URL = 'https://learnhive.vercel.app/'
 console.log("SOCKET_URL from socket.ts: ", SOCKET_URL);
 
 const socket: Socket = io(SOCKET_URL, {
